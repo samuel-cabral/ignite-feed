54e0f4a86aaa6d172a134ff60ed5c64b71c202ad
Samuel Cabral <samuelcabral.mail@gmail.com>
2023-12-14 01:26:10 -0300

    feat: css global

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Post } from './Post'
+import { Header } from './components/Header'
+
+import './global.css'
 
 export function App() {
   return (
     <div>
-      <Post 
-        author="Samuel Cabral" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, placeat. Excepturi nulla velit perferendis distinctio nisi iusto repellat eius sunt fugiat omnis quae fugit odit nesciunt expedita odio, quaerat dolores." 
+      <Header />
+      <Post
+        author="Samuel Cabral"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, placeat. Excepturi nulla velit perferendis distinctio nisi iusto repellat eius sunt fugiat omnis quae fugit odit nesciunt expedita odio, quaerat dolores."
+      />
+      <Post
+        author="Mayrla Cabral"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, placeat. Excepturi nulla velit perferendis distinctio nisi iusto repellat eius sunt fugiat omnis quae fugit odit nesciunt expedita odio, quaerat dolores."
       />
     </div>
-    
   )
 }
