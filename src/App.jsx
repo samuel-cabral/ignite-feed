d8dd4fb9bd97f6a79cf5ce5b33c4bb02428e1cb4
Samuel Cabral <samuelcabral.mail@gmail.com>
2023-12-13 23:24:46 -0300

    feat: start a project named ignite-feed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Post } from './Post'
+
+export function App() {
+  return (
+    <div>
+      <Post 
+        author="Samuel Cabral" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, placeat. Excepturi nulla velit perferendis distinctio nisi iusto repellat eius sunt fugiat omnis quae fugit odit nesciunt expedita odio, quaerat dolores." 
+      />
+    </div>
+    
+  )
+}
