342b44511b62625bc359192da974b150313ad4be
Samuel Cabral <samuelcabral.mail@gmail.com>
2023-12-15 17:43:42 -0300

    feat: finish sidebar ui component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import { Post } from './Post'
 import { Header } from './components/Header'
 
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Samuel Cabral"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, placeat. Excepturi nulla velit perferendis distinctio nisi iusto repellat eius sunt fugiat omnis quae fugit odit nesciunt expedita odio, quaerat dolores."
-      />
-      <Post
-        author="Mayrla Cabral"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, placeat. Excepturi nulla velit perferendis distinctio nisi iusto repellat eius sunt fugiat omnis quae fugit odit nesciunt expedita odio, quaerat dolores."
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Diego Fernandes" content="Criando um app CRA do zero" />
+          <Post author="Diego Fernandes" content="Criando um app CRA do zero" />
+          <Post author="Diego Fernandes" content="Criando um app CRA do zero" />
+        </main>
+      </div>
     </div>
   )
 }
